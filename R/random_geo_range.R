0afee0d3709fe74407fb9f9c8a6c26449b1ec5f1
samuelonso <changalfsam@gmail.com>
2020-06-22 00:52:23 -0400

    updated format

--- R/random_geo_range.R
+++ R/random_geo_range.R
@@ -17,45 +17,48 @@
 #' @param taxa_col Name of column of taxa (species) values. Caps sensitive.
 #'
 
-random_geo_range <- function(n_length, occs_df, lat_col = "latitude", lon_col = "longitude",
-                             lat_uncertainty = "lat_uncertainty",
-                             lon_uncertainty = "lon_uncertainty",
-                             taxa_col = "species",...)
-{
-  rand_EOOs = c()
-  rand_AOOs = c()
-
-  for (i in 1:n_length) {
-    set.seed(i)
-    occ_random <- generate_occ_uncertain(occs_df, 
-      lat_col = lat_col, 
-      lon_col = lon_col,
-      lat_uncertainty = lat_uncertainty,
-      lon_uncertainty = lon_uncertainty,
-      taxa_col = taxa_col)
+random_geo_range <-
+  function(n_length,
+    occs_df,
+    lat_col = "latitude",
+    lon_col = "longitude",
+    lat_uncertainty = "lat_uncertainty",
+    lon_uncertainty = "lon_uncertainty",
+    taxa_col = "species",
+    ...)
+  {
+    rand_EOOs = c()
+    rand_AOOs = c()
     
-    
+    for (i in 1:n_length) {
+      set.seed(i)
+      occ_random <- generate_occ_uncertain(
+        occs_df,
+        lat_col = lat_col,
+        lon_col = lon_col,
+        lat_uncertainty = lat_uncertainty,
+        lon_uncertainty = lon_uncertainty,
+        taxa_col = taxa_col
+      )
+      
+      
       # Calculate EOO
-    EOO_temp <-
-      EOO.computing(
-        occ_random,
-        exclude.area = T,
-        country_map = land,
-        write_results = F
-      )$EOO
+      EOO_temp <-
+        EOO.computing(
+          occ_random,
+          exclude.area = T,
+          country_map = land,
+          write_results = F
+        )$EOO
       # Add new EOO value to rand_EOOs
       rand_EOOs <- c(rand_EOOs, EOO_temp)
-
+      
       # Calculate AOO
       AOO_temp <- AOO.computing(occ_random)
       # Add new AOO value to rand_AOOs
       rand_AOOs <- c(rand_AOOs, AOO_temp)
-    
-
+      
+      
+    }
+    return (data.frame(rand_EOO = rand_EOOs, rand_AOO = rand_AOOs))
   }
-  return (data.frame(rand_EOO = rand_EOOs, rand_AOO = rand_AOOs))
-}
-
-
-
-
