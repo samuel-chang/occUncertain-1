dd41dc329eba38d640cc4c62ff101f8850d97f0a
samuelonso <changalfsam@gmail.com>
2020-11-08 22:00:59 -0500

    protected areas and ecoregion

---
+++ R/ecoregion.R
@@ -0,0 +1,12 @@
+#' ecoregion
+#'
+#' A SpatialPolygonDataFrame showing terrestial ecoregions
+#'
+#'
+#' @keywords datasets
+#' @name ecoregion
+#' @usage data(ecoregion)
+#' @format A dataframe
+#' @source \url{https://www.globildata.org/arcgis/rest/services/Global/Terrestrial_Ecoregions_World/MapServer/0/query?outFields=*&where=1%3D1}
+NULL
+
