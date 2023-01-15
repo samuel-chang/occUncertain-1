c902dc0bdd6c7e438a67f4d096d6db153e5da6ee
samuel-chang <41313251+samuel-chang@users.noreply.github.com>
2023-01-15 12:28:51 -0500

    updated data set API download link and package description

--- R/Leopardus_wiedii_gbif.R
+++ R/Leopardus_wiedii_gbif.R
@@ -1,8 +1,8 @@
 #' Occurence data for \emph{Leopardus wiedii} acquired from GBIF
 #'
-#' The margay (Leopardus wiedii, Schinz 1821) is a small Neotropical cat that is native with distribution from South America into Northeastern Mexico.  Citation - GBIF.org (24 March 2020) GBIF Occurrence Download https://doi.org/10.15468/dl.5xb5f9
+#' The margay (Leopardus wiedii, Schinz 1821) is a small Neotropical cat that is native with distribution from South America into Northeastern Mexico.  Citation - GBIF.org (January 2023) GBIF Occurrence Download http://api.gbif.org/v1/occurrence/download/request/0066556-200221144449610.zip
 #'
 #' DOI 10.15468/dl.ljhrm9
-#' @source \url{http://www.gbif.org}
+#' @source \url{https://www.gbif.org}
 "Leopardus_wiedii_gbif"
 
